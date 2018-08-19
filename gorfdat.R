a32f2782f3b76ba7b8794860c1322da756eb29e9
AssefaJo <assefa.johannes@yahoo.de>
2018-08-19 15:47:16 +0200

    asd

--- gorfdat.R
+++ gorfdat.R
@@ -1,6 +1,62 @@
 library(matlib)
 library(shapes)
 library(Morpho)
+norm_vec <- function(x) sqrt(sum(x^2))
+
+#Gibt die euklidische Norm eines 2d-arrays zurückkk
+norm_arr <- function(x) norm_vec(as.vector(x))
+
+expo <- function(x,v){
+  
+  k <- nrow(x)
+  m <- ncol(x)
+  
+  #x und v zu Vektor umschreiben
+  x_vec <- as.vector(x)
+  v_vec <- as.vector(v)
+  
+  #Norm von v und x
+  nv <- norm_vec(v_vec)
+  nx <- norm_vec(x_vec)
+  
+  #Berechnung des k*m dimensionalen shapes als vektor
+  e <- cos(nv)*x_vec+((sin(nv)*nx)/nv)*v_vec
+  
+  matrix(e, nrow = k, ncol = m, byrow=FALSE)
+}  
+
+
+#Riemannscher Logarithmus
+loga <- function(x,y){
+  
+  k <- nrow(x)
+  m <- ncol(x)
+  
+  #x und y zu Vektor umschreiben
+  x_vec <- as.vector(x)
+  y_vec <- as.vector(y)
+  
+  #Norm von y und x
+  ny <- norm_vec(y_vec)
+  nx <- norm_vec(x_vec)
+  
+  #y normieren
+  
+  #y_vec <- y_ve/n
+  #ny <- norm_vec(y_vec)
+  
+  #theta
+  t <- acos(abs(sum(x_vec*y_vec))/(nx*ny))
+  
+  #Projektion von y auf x
+  pi <- (x_vec*sum(x_vec*y_vec))/(nx^2)
+  
+  #Berechnung und Ausgabe des Vektors im Tangentialraum von x
+  vec<-(t*(y_vec-pi))/norm_vec(y_vec-pi)
+  
+  #Umwandlung in ein 2D array
+  matrix(vec, nrow = k, ncol = m, byrow=FALSE)
+}
 
 
 data(gorf.dat)
@@ -9,15 +65,48 @@ proc <- procSym(gorf.dat,orp=TRUE,scale=F)
 
 x <- proc$mshape
 
-#Folgende zwei plots möchte ich in einem Koordinatensystem darstellen
-#deformgrid2d liefert mir die Darstellung von lediglich 2 shapes
-plotshapes(proc$rotated[,,1:10],color = "red")
-plotshapes(x,color = "green")
+N<-10
+#Anzahl der Koordinaten der shapes 
+d<-length(x)
+#Dimension des Shape-Space
+c<-length(proc$PCs[1,])
+
+r<-0.06
+
+#Plots für den Vergleich von unterschiedlichen Standardabweichungen
+e<-array(0,c(c,N))
+
+for(i in 1:N){e[,i]<-as.vector(rnorm(c,mean=0,sd=1))}
+
+#c\times c Kovarianzmatrix
+cov <- diag(rep(r,times=c))
+#E ist zu N(0,cov*t(cov)) verteilt
+E<-array(0,c(c,N)) 
+for(i in 1:N){E[,i]<-cov%*%e[,i]}
+
+#Basis von der tangentialen Hypereben an der Mean Shape
+b <- proc$PCs
+
+t<-array(0,c(d/2,2,N))
+for(j in 1:N){for(i in 1:c){t[,,j]<-t[,,j]+(b[,i]*E[i,j])}}
+
+z <- array(0,c(d/2,2,N))
+#N shapes auf Shape Space
+for(i in 1:N){z[,,i]<-expo(x,t[,,i])}
+
+
+#Plots:
+
+#4.2
+plotshapes(z)
+points(x,col="green",pch=19)
+plotshapes(proc$rotated[,,1:N])
+points(x,col="green",pch=19)
+
+#2.1
+plotshapes(gorf.dat[,,1:N])
+plotshapes(proc$rotated[,,1:N])
+points(x,col="green",pch=19)
 
 
 
-
-
-
-#deformGrid2d(proc$orpdata[,,3],x+loga(x,proc$rotated[,,3]),wireframe = c(1,6:2,8:6))
-#deformGrid2d(x,proc$rotated[,,1],wireframe = c(1,6:2,8:6))
---
+++ pca.R
@@ -0,0 +1,162 @@
+#Exponentialabbilung
+expo <- function(x,v){
+  
+  #Norm von v und x
+  nv <- sqrt(sum(v^2))
+  nx <- sqrt(sum(x^2))
+  
+  #Berechnung des Pre-Shape auf S^k_m
+  pshape <- cos(nv)*x+((sin(nv)*nx)/nv)*v
+  
+  return(pshape)
+}  
+
+
+#Riemannscher Logarithmus
+loga <- function(x,y){
+  
+  #Norm von y und x
+  ny <- sqrt(sum(y^2))
+  nx <- sqrt(sum(x^2))
+  
+  #theta
+  t <- acos(abs(sum(x*y))/(nx*ny))
+  
+  #Projektion von y auf x
+  pi <- (x*sum(x*y))/(nx^2)
+  
+  #Berechnung des Pre-Shape Vektors
+  pshape_v <- (t*(y-pi))/sqrt(sum((y-pi)^2))
+  
+  return(pshape_v)
+}
+
+pca <- function(N,dimL=2,Landmarks=3,r=0.2){
+  
+  if(Landmarks<=dimL) {
+    return(message("Die Anzahl der Landmarks muss groesser sein als die Dimension der Landmarks"))
+  }
+  
+  #Dimension des umliegenden Raumes
+  d = dimL * (Landmarks-1)
+  
+  #Dimension des Pre-Shape Space/Tangentialraums
+  c = d - 1
+  
+  #Parameter Sigma, den es zu schaetzen gilt
+  Sigma <- diag(rep(r,times=c))
+  #Generiere array e mit N standardnormalverteilten c-dimensionalen Vektoren
+  e<-array(0,c(c,N))
+  
+  for(i in 1:N) {
+    e[,i] <- as.vector(rnorm(c,mean=0,sd=1))
+  }
+  
+  #Multiplikation mit Sigma
+  E<-array(0,c(c,N))
+  
+  for(i in 1:N) {
+    E[,i] <- Sigma %*% e[,i]
+  }
+  #Mean Shape x als "Nordpol" der Einheitssphaere
+  x <- array(c(rep(0,times=c),1))
+  
+  #In Koordinaten dargestellte orthonormale Basis des Tangentialraums an x
+  b <- diag(rep(1,times=d))[,-d]
+  
+  #Vektoren erzeugen als Linearkombination der Basis b und den Eintraegen von E als Koeffizienten
+  v <- array(0,c(d,N))
+  
+  for(j in 1:N) {
+    for(i in 1:c) {
+      v[,j] <- v[,j] + (b[,i] * E[i,j])
+    }
+  }
+  
+  #Falls die Laenge der Vektoren laenger als pi/2=1.57 wird, macht eine orthogonale Projektion keinen Sinn.
+  for(i in 1:N) {
+    if(sqrt(sum(v[,i]^2))>pi/2) {
+      return(message("Standardabweichung zu gross"))
+    }
+  }
+  
+  #Anwendung der Exponentialabbildung liefert Pre-Shapes ps auf M
+  ps <- array(0,c(d,N))
+  
+  for(i in 1:N) {
+    ps[,i] <- expo(x,v[,i])
+  }
+  
+  
+  #Der mit log bestimmte exakte Wert von ps im Tangentialraum. 
+  ps_exakt <- array(0,c(d,N))
+  
+  for(i in 1:N) {
+    ps_exakt[,i] <- loga(x,ps[,i])+x
+  }
+  
+  #Projiziere ps orthogonal in den Tangentialraum von x.
+  ps_orp <- ps
+  
+  for (i in 1:N) {
+    ps_orp[d,i] <- 1
+  }
+  
+  #Orthogonal projizierter Vektor
+  v_orp <- array(0,c(d-1,N))
+  
+  for (i in 1:N) {
+    v_orp[,i] <- ps_orp[,i][-d]
+  }
+  
+  #Exakt projizierter Vektor
+  v_exakt <- array(0,c(d-1,N))
+  
+  for (i in 1:N) {
+    v_exakt[,i] <- ps_exakt[,i][-d]
+  }
+  
+  #Berechnen der Kovarianzmatrix der exakten Daten
+  cov_exakt <- matrix(0,nrow = c,ncol = c)
+  
+  for(i in 1:N) {
+    cov_exakt <- cov_exakt + (1/N) * (v_exakt[,i] %*% t(v_exakt[,i]))
+  }
+  
+  
+  #Berechnen der Kovarianzmatrix der approximierten Daten
+  cov_orp <- matrix(0,nrow = c,ncol = c)
+  
+  for(i in 1:N) {
+    cov_orp <- cov_orp + (1/N) * (v_orp[,i] %*% t(v_orp[,i]))
+  }
+  
+  #Singulaerwertzerlegung
+  EDe <- eigen(cov_exakt)
+  
+  EDo <- eigen(cov_orp)
+  
+  Sigma_ex <- EDe$vectors %*% diag(sqrt(EDe$values)) %*% t(EDe$vectors)
+  Sigma_ap <- EDo$vectors %*% diag(sqrt(EDo$values)) %*% t(EDo$vectors)
+  
+  D <- Sigma_ex - Sigma_ap
+  
+  #Abweichung der Schaetzungen
+  Delta <- sqrt(max(eigen(t(D) %*% D)$values))
+  
+  out = (list(Sigma = Sigma , Sigma_ex = Sigma_ex , Sigma_ap = Sigma_ap , Differenz = Delta))
+  
+  return(out)
+}
+
+
+#Plots:
+
+#4.1
+deriv<-seq(0.01,0.35,0.002)
+x<-rep(0,times=length(deriv))
+for (k in 1:length(deriv)) {
+  x[k]<-pca(1000,r = deriv[k])$Differenz
+}
+plot(deriv,x,xlab="r",ylab=expression(Delta^r))
+
